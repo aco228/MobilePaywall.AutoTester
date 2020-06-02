fe99ebaaa7faea66b054a6bdd15af4c522077a58
aco228 <35331284+aco228@users.noreply.github.com>
2020-06-02 22:34:50 +0200

    Initial commit

---
+++ MobilePaywall.AutoTester/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MobilePaywall.AutoTester.Controllers
+{
+	public class HomeController : Controller
+	{
+
+		public ActionResult Index()
+		{
+			return this.Content("OK");
+		}
+
+	}
+}
